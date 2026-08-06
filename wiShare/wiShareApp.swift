ed43e49f7112165793b4329264e122e017333ae4
viscontti <nihonto.ps4@gmail.com>
2026-08-06 19:51:34 +0300

    Initial Commit

---
+++ wiShare/wiShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  wiShareApp.swift
+//  wiShare
+//
+//  Created by n viscontti on 06.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct wiShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
